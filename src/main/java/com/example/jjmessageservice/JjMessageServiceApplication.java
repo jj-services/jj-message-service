daaf67b0f79843f65388814373d04aeb278f13ac
tomasgp97 <tomas.gonzalezp97@gmail.com>
2021-06-29 04:05:09 -0300

    first commit

---
+++ src/main/java/com/example/jjmessageservice/JjMessageServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.jjmessageservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjMessageServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JjMessageServiceApplication.class, args);
+    }
+
+}
